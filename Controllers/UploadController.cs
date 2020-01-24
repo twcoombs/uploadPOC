43cb4e062c8e1d3487dc33fe102d635e93b7d88a
Tim Coombs <twcoombs@gmail.com>
2020-01-24 20:46:38 +1300

    first commit of poc

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace uploadPOC.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            // Extract file name from whatever was posted by browser
+            var fileName = System.IO.Path.GetFileName(file.FileName);
+
+            // If file with same name exists delete it
+            if (System.IO.File.Exists(fileName))
+            {
+                System.IO.File.Delete(fileName);
+            }
+
+            // Create new local file and copy contents of uploaded file
+            using (var localFile = System.IO.File.OpenWrite(fileName))
+            using (var uploadedFile = file.OpenReadStream())
+            {
+                uploadedFile.CopyTo(localFile);
+            }
+
+            ViewBag.Message = "File successfully uploaded";
+
+            return View();
+        }
+    }
+}
